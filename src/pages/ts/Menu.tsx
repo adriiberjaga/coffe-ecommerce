e2a40376fda44e1c37d690c37d20595ef933d2df
adriiberjaga <adriabdd@gmail.com>
2025-05-13 11:53:29 +0200

    .

--- src/pages/ts/Menu.tsx
+++ src/pages/ts/Menu.tsx
@@ -5,16 +5,36 @@ import '../css/Menu.css';
 
 export default function Menu() {
   const [category, setCategory] = useState('todos');
+  const [order, setOrder] = useState('ordenar');
 
   const filteredProducts = category === 'todos'
     ? MenuProducts
     : MenuProducts.filter(product => product.category.toLowerCase() === category);
+
+  const orderProducts = (products: any) => {
+    if (order === 'menor') {
+      return [...products].sort((a, b) => a.price - b.price);
+    } else if (order === 'mayor') {
+      return [...products].sort((a, b) => b.price - a.price);
+    } else {
+      return products;
+    }
+  };
+
+  const orderedFilteredProducts = orderProducts(filteredProducts);
 
   return (
     <div>
       <BannerMenu />
       <section className="menu-section">
         <div className="menu-categories">
+          <select title={order} name={order} value={order} onChange={(e) => setOrder(e.target.value)}>
+            <option value="ordenar">Ordenar por:</option>
+            <option value="normal">Más comprados</option>
+            <option value="menor">Precio: menor a mayor</option>
+            <option value="mayor">Precio: mayor a menor</option>
+          </select>
+
           {['todos', 'en grano', 'molido', 'cápsulas', 'accesorios'].map((cat) => (
             <button
               key={cat}
@@ -27,7 +47,7 @@ export default function Menu() {
         </div>
 
         <div className="menu-products">
-          {filteredProducts.map((product, index) => (
+          {orderedFilteredProducts.map((product: any, index: number) => (
             <div key={index} className="menu-product-card">
               <img src={product.image} alt={product.title} className="product-image" />
               <h5 className="product-name">{product.title}</h5>
